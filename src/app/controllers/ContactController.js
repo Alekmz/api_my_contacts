474cb4bc3d67bb703cb5e0693ec10da3e5cdf8aa
Alekmz <amalexmuniz@gmail.com>
2023-01-22 17:42:46 -0300

    feat: create endpoints get and delete

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,14 +1,21 @@
 const ContactsRepository = require("../repositories/ContactsRepository");
 
 class ContactController {
-  index(req, res) {
+  async index(req, res) {
     // List all
-    const contacts = ContactsRepository.findAll();
-    res.json(contacts)
+    const contacts = await ContactsRepository.findAll();
+    res.json(contacts);
   }
 
-  show() {
-    // Get register with id for example
+  async show(req, res) {
+    const { id } = req.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    res.json(contact);
   }
 
   store() {
@@ -19,8 +26,15 @@ class ContactController {
     // Update register
   }
 
-  delete() {
-    // Delete register
+  async delete(req, res) {
+    const { id } = req.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+    await ContactsRepository.delete(id);
+    res.sendStatus(204);
   }
 }
-module.exports =  new ContactController();
+module.exports = new ContactController();
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,18 +1,38 @@
-const { uuid } = require("uuidv4");
+const { v4 } = require("uuid");
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: "Alex",
     email: "example@gmail.com",
     phone: "123123123",
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: "Joao",
+    email: "example@gmail.com",
+    phone: "56456",
+    category_id: v4(),
   },
 ];
 
 class ContactsRepository {
   findAll() {
-    return contacts;
+    return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) =>
+      resolve(contacts.find((contact) => contact.id === id))
+    );
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
